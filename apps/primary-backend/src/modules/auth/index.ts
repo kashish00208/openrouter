589f84603455a33ebda16bc95168030ff4b8a896
Kashish <kgupta7827@gmail.com>
2026-04-08 23:46:22 +0530

    completion backend init

--- apps/primary-backend/src/modules/auth/index.ts
+++ apps/primary-backend/src/modules/auth/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { Response, Request } from 'express'
 const app = express()
-import {prisma} from ''
 app.get("/sign-in", async (req: Request, res: Response) => {
     try{
 
